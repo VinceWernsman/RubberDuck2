cce968a973d7f76a4d79d78f69878575f20f2117
Vince Wernsman <vincewernsman@Vinces-MacBook-Pro.local>
2022-02-14 14:09:07 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int question = 501;
+        if (question > 500) {System.out.println("Simplify your question");}
+        if (question < 200) {System.out.println("Ask a better question");}
+        if (question >200 && question <500) {System.out.println("Good Job!");}
+    }
+}
